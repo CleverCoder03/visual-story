74a40c2a87ba143adb76ac1f466705ffb9d1ee81
Senpai <clevercoder0307@gmail.com>
2025-10-21 11:46:29 +0530

    developed Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
   {
@@ -33,20 +35,38 @@ const navLinks = [
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const pathName = usePathname()
+
+  useEffect(() => {
+    if (toggle) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "unset";
+    }
+    return () => {
+      document.body.style.overflowY = "unset";
+    };
+  }, [toggle]);
   return (
     <>
-      <div className="fixed z-5 w-full px-6 py-3">
-        <div className="relative flex items-center justify-between">
+      <div className="fixed z-5 w-full px-6 md:px-10 lg:px-18 xl:px-28 py-3 h-22 bg-white shadow-sm">
+        <div className="relative w-full h-full flex items-center justify-between">
           <Link href="/">
             <div className="relative size-20">
               <Image src="/logo.png" alt="logo" fill />
             </div>
           </Link>
 
-          <div></div>
+          <div className="hidden lg:flex lg:gap-6 xl:gap-10 font-sans font-medium">
+            {navLinks.map((link) => (
+                <Link key={link.id} href={link.link} className={`${pathName === link.link ? "underline decoration-1 underline-offset-4" : ""}`}>
+                  {link.name}
+                </Link>
+              ))}
+          </div>
 
           <div
-            className=" cursor-pointer font-sans font-bold text-xl"
+            className="lg:hidden cursor-pointer font-sans font-bold text-xl"
             onClick={() => setToggle(!toggle)}
           >
             {toggle ? "Close" : "Menu"}
@@ -54,17 +74,22 @@ const Navbar = () => {
         </div>
       </div>
 
-      {toggle && (
-        <div className="absolute z-4 top-0 left-0 h-full w-full bg-white flex justify-center items-center">
-          <div className="flex justify-center items-center flex-col gap-8 font-bold text-2xl">
-            {navLinks.map((link) => (
-              <Link key={link.id} href={link.link}>
-                {link.name}
-              </Link>
-            ))}
-          </div>
-        </div>
-      )}
+        {toggle && (
+          <motion.div
+            initial={{ y: "-100%" }}
+            animate={{ y: 0 }}
+            transition={{ ease: "easeInOut", duration: 0.5 }}
+            className="absolute z-4 top-0 left-0 h-full w-full bg-white flex justify-center items-center"
+          >
+            <div className="flex justify-center items-center flex-col gap-8 font-bold text-2xl">
+              {navLinks.map((link) => (
+                <Link key={link.id} href={link.link} className={`${pathName === link.link ? "underline decoration-2 underline-offset-6" : ""}`} aria-label={link.name}>
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+          </motion.div>
+        )}
     </>
   );
 };
