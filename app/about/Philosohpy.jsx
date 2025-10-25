58cf0e3df957e17228828663d2b88f204ee9079b
Senpai <clevercoder0307@gmail.com>
2025-10-25 18:57:14 +0530

    added philosophy.jsx

---
+++ app/about/Philosohpy.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+
+const Philosohpy = () => {
+  return (
+    <div className="py-15 lg:py-20">
+      <div className="hidden lg:flex justify-center">
+        <div className="overflow-hidden relative w-90 h-80 shadow-2xl rounded-md">
+          <Image
+            src="/approach.jpg"
+            alt="Approach"
+            className="object-cover"
+            fill
+          />
+        </div>
+      </div>
+
+      <div className="">
+        <div className="text-pink-600 text-center [&>h1]:text-3xl lg:[&>h1]:text-4xl [&>h1]:font-serif font-semibold">
+          <h1>Our Philosophy</h1>
+          <p>Documentary Artistry</p>
+        </div>
+        <p className="mt-5 text-center font-sans lg:text-lg">
+          At Visual Story, our work is driven by a desire to capture the
+          authentic soul of a moment, transforming it into a piece of art that
+          will stand the test of time. We see ourselves as visual narrators,
+          blending the artistry of documentary photography with a fine-art
+          sensibility. We focus on candid moments and natural light to create
+          images that are both authentic and beautiful. We believe in being an
+          unobtrusive presence, allowing you to be fully present in your moments
+          while we take care of preserving them forever.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Philosohpy;
