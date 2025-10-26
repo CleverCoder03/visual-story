7f58c644ad95fcf4775172dad38bc4082259e247
Senpai <clevercoder0307@gmail.com>
2025-10-26 18:06:23 +0530

    responsive contact page

--- app/contact/ContactForm.jsx
+++ app/contact/ContactForm.jsx
@@ -6,7 +6,9 @@ const ContactForm = () => {
   return (
     <div className="px-6 md:px-10 lg:px-18 xl:px-28 mt-5">
       <div className="flex flex-col items-center">
-        <Headline title={"Get In Touch"} />
+        <div className="text-pink-600 text-center [&>h1]:text-3xl lg:[&>h1]:text-4xl [&>h1]:font-serif py-10 font-semibold">
+          <h1>Get in touch</h1>
+        </div>
         <p className="text-center max-w-2xl font-sans ">
           For project inquiries, please fill out the form below. We'll get back
           to you within 24 hours. For all other questions, feel free to reach
